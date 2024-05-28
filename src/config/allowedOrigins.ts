1788e0b3fc72ef28ddba40d1125c8a7b181cfa36
Precious Adedibu <adedibuprecious@gmail.com>
2024-05-28 12:53:50 +0100

    update

--- src/config/allowedOrigins.ts
+++ src/config/allowedOrigins.ts
@@ -9,7 +9,7 @@ const allowedOrigins = [
   "https://dunecar-deploy.vercel.app",
   "https://dunecar-admin-p3pnyjfy5-preshy-jones.vercel.app",
   "https://dunecar-admin.vercel.app",
-  "http://ec2-13-48-123-182.eu-north-1.compute.amazonaws.com:3000/"
+  "http://ec2-13-48-123-182.eu-north-1.compute.amazonaws.com:3000"
 ];
 
 export default allowedOrigins;
